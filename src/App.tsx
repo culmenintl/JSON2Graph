3c4268e8a573d4f7b059ab101c4c886405e635f4
hzyIO <loganjhendershot@gmail.com>
2022-09-30 16:45:20 -0700

    feat(demo): impl of sigmajs demo code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import Root from "./components/Root";
 import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="text-center selection:bg-green-900">
-      <header className="flex min-h-screen flex-col items-center justify-center bg-[#282c34] text-white">
-        <img
+      <header className="flex min-h-screen flex-col items-center justify-center bg-[#282c34]">
+        {/* <img
           src={logo}
           className="animate-speed h-60 motion-safe:animate-spin"
           alt="logo"
@@ -17,7 +18,8 @@ function App() {
             }\
           "
           }
-        </style>
+        </style> */}
+        <Root />
       </header>
     </div>
   );
