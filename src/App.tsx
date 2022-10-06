385168ef09414a66ab15b10a6894ab99b55817e0
hzyIO <loganjhendershot@gmail.com>
2022-10-05 17:24:51 -0700

    fix(ui): updated zoom min,max linted code and updated click functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
-import Root from "./components/Root";
-import logo from "./logo.svg";
+import Root from './components/Root';
 
 function App() {
-  return (
-    <div className="text-center selection:bg-green-900">
-      <header className="flex min-h-screen flex-col items-center justify-center bg-[#282c34]">
-        {/* <img
-          src={logo}
-          className="animate-speed h-60 motion-safe:animate-spin"
-          alt="logo"
-        />
-        <style>
-          {
-            "\
-            .animate-speed{\
-              animation-duration:20s;\
-            }\
-          "
-          }
-        </style> */}
-        <Root />
-      </header>
-    </div>
-  );
+    return (
+        <div className="text-center selection:bg-green-900">
+            <header className="flex min-h-screen flex-col items-center justify-center bg-[#282c34]">
+                <Root />
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,32 +1,32 @@
 export interface NodeData {
-  key: string;
-  label: string;
-  tag: string;
-  URL: string;
-  cluster: string;
-  x: number;
-  y: number;
+    key: string;
+    label: string;
+    tag: string;
+    URL: string;
+    cluster: string;
+    x: number;
+    y: number;
 }
 
 export interface Cluster {
-  key: string;
-  color: string;
-  clusterLabel: string;
+    key: string;
+    color: string;
+    clusterLabel: string;
 }
 
 export interface Tag {
-  key: string;
-  image: string;
+    key: string;
+    image: string;
 }
 
 export interface Dataset {
-  nodes: NodeData[];
-  edges: [string, string][];
-  clusters: Cluster[];
-  tags: Tag[];
+    nodes: NodeData[];
+    edges: [string, string][];
+    clusters: Cluster[];
+    tags: Tag[];
 }
 
 export interface FiltersState {
-  clusters: Record<string, boolean>;
-  tags: Record<string, boolean>;
+    clusters: Record<string, boolean>;
+    tags: Record<string, boolean>;
 }
