5d651f83c37a8b8a17e2d3cd4eb7b7e50fab1147
hzyIO <loganjhendershot@gmail.com>
2022-10-21 13:45:08 -0700

    feat(data): added sample dataset to repo, fixed button onClick

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,13 +2,14 @@ import { FC } from 'react';
 
 interface Props {
     text: string;
-    onPress?: () => void;
+    onClick?: () => void;
 }
-const Button: FC<Props> = ({ text }) => {
+const Button: FC<Props> = ({ text, onClick: onClick }) => {
     return (
         <button
             type="button"
             className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+            onClick={onClick}
         >
             {text}
         </button>
