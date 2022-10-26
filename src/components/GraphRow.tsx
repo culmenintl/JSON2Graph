90823aa03ef33656907f122444cbc63f3e8e45cf
hzyIO <loganjhendershot@gmail.com>
2022-10-26 13:27:30 -0700

    fix(ui): updated naming scheme for graphRow

--- src/components/GraphRow.tsx
+++ src/components/GraphRow.tsx
@@ -3,12 +3,13 @@ import { FC } from 'react';
 type Props = {
     label: string;
     value?: String | JSX.Element | React.ReactNode;
-    pre?: React.ReactNode;
+    preRendered?: React.ReactNode;
 };
-export const GraphRow: FC<Props> = ({ label, value, pre }) => {
+
+export const GraphRow: FC<Props> = ({ label, value, preRendered }) => {
     return (
-        <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
-            <dl className="sm:divide-y sm:divide-gray-200">
+        <div className="border-t border-gray-300 px-4 py-5 sm:p-0">
+            <dl className="sm:divide-y sm:divide-gray-300">
                 <div className="items-center py-4 text-left sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
                     <dt className="text-sm font-medium text-gray-500">
                         {label}
@@ -18,9 +19,9 @@ export const GraphRow: FC<Props> = ({ label, value, pre }) => {
                             {value}
                         </dd>
                     )}
-                    {pre && (
-                        <dd className="mt-1 overflow-x-clip text-xs text-gray-900 sm:col-span-2 sm:mt-0">
-                            {pre ? pre : null}
+                    {preRendered && (
+                        <dd className="mt-1 overflow-x-hidden text-xs text-gray-900 sm:col-span-2 sm:mt-0">
+                            {preRendered ? preRendered : null}
                         </dd>
                     )}
                 </div>
