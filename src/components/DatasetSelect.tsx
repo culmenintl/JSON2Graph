2e05142cb96539345130d67243b741cc4be14221
hzyIO <loganjhendershot@gmail.com>
2022-11-07 15:52:31 -0800

    fix(ui): data select fix updating whats currently selected

--- src/components/DatasetSelect.tsx
+++ src/components/DatasetSelect.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { observer } from 'mobx-react-lite';
 import { RootStoreModel } from '../stores/RootStore';
 import useInject from '../hooks/useInject';
+import { STATUS } from '../stores/AppStore';
 
 const mapStore = ({ dataStore, appStore }: RootStoreModel) => ({
     appStore,
@@ -13,12 +14,13 @@ export const DatasetSelect: FC<{}> = observer(() => {
     return (
         <div>
             <select
-                id="location"
-                name="location"
                 className="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                value={dataStore.datasetIndex}
                 onChange={async (e) => {
                     console.log('onChange');
                     appStore.toggleDevMode();
+                    appStore.setStatus(STATUS.FETCHING);
+                    appStore.setLoading(true);
                     dataStore.setDatasetIndex(parseInt(e.target.value));
                     await dataStore.fetchData();
                 }}
