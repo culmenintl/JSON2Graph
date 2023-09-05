cd40c84fcab21b9acb4c4ce98f528d4f024d50f8
Logan Hendershot <logan@lvx3.com>
2023-09-05 11:26:37 -0700

    fix(build): updates for package and smaller sample sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     content: [
-        "index.html",
-        "./src/**/*.{js,jsx,ts,tsx,html}",
+        "./index.html",
+        "./src/**/*.{js,jsx,ts,tsx}",
         "node_modules/daisyui/dist/**/*.js",
         "node_modules/react-daisyui/dist/**/*.js",
     ],
