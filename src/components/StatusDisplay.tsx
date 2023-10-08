1bbdf976c821aa9b028598a87e840adc88c89a85
Logan Hendershot <logan@lvx3.com>
2023-10-08 09:49:46 -0700

    build(fix): removed unused component

--- src/components/StatusDisplay.tsx
+++ src/components/StatusDisplay.tsx
@@ -3,34 +3,35 @@ import React, { useEffect, useRef, useState } from "react"
 import { useTrackedStore } from "../stores/Store"
 
 export const StatusDisplay: React.FC = () => {
-    const parent = useRef(null)
-    const status = useTrackedStore().app.status()
-    const [localStatus, setLocalStatus] = useState<string[]>([])
+    return <></>
+    // const parent = useRef(null)
+    // const status = useTrackedStore().app.status()
+    // const [localStatus, setLocalStatus] = useState<string[]>([])
 
-    useEffect(() => {
-        parent.current && autoAnimate(parent.current)
-    }, [parent])
+    // useEffect(() => {
+    //     parent.current && autoAnimate(parent.current)
+    // }, [parent])
 
-    useEffect(() => {
-        console.log("new value:", status)
-        if (!localStatus.length) {
-            setLocalStatus([status])
-        } else {
-            setLocalStatus([])
-            setTimeout(() => {
-                setLocalStatus([status])
-            }, 500)
-        }
+    // useEffect(() => {
+    //     console.log("new value:", status)
+    //     if (!localStatus.length) {
+    //         setLocalStatus([status])
+    //     } else {
+    //         setLocalStatus([])
+    //         setTimeout(() => {
+    //             setLocalStatus([status])
+    //         }, 500)
+    //     }
 
-        // update value in 500 ms
-    }, [status])
-    return (
-        <div ref={parent} className="flex flex-1 flex-col">
-            {localStatus.map((val) => (
-                <span key={val} className="">
-                    {val}
-                </span>
-            ))}
-        </div>
-    )
+    //     // update value in 500 ms
+    // }, [status])
+    // return (
+    //     <div ref={parent} className="flex flex-1 flex-col">
+    //         {localStatus.map((val) => (
+    //             <span key={val} className="">
+    //                 {val}
+    //             </span>
+    //         ))}
+    //     </div>
+    // )
 }
